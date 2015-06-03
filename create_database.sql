aafd0312c73211c7e1f1ff5fbda5fcc7629172ad
MiJo0623 <mijo2005@web.de>
2015-06-03 11:05:13 +0200

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,15 @@
+drop table Beziehung_Kunde_Gesuch;
+drop table Beziehung_kunde_angebot;
+drop table Beziehung_Strecken;
+drop table Beziehung_Vermittlung;
+drop table angebot;
+drop table gesuch;
+drop table auto;
+drop table mitarbeiter;
+drop table kunde;
+drop table ort;
+
+
 
 
 CREATE TABLE Mitarbeiter(
